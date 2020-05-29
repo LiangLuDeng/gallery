b79222eaa45bb066ea4029b27769a358d304b71e
qinhui <932433511@qq.com>
2020-05-29 17:48:20 +0800

    feat: 开发首页的布局

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import styles from './index.less';
 
+import { Row, Col } from 'antd';
+
 export default () => {
-  return <div>this is content</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles['left-content']}>left content</div>
+      </div>
+      <div className={styles.right}>right</div>
+    </div>
+  );
 };
